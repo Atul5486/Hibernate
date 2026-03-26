e118a085de1888b1ff035c8aa4bf2d77be87c1f3
Atul5486 <atulkoushal5486@gmail.com>
2026-03-26 18:44:38 +0530

    Many-to-Many

---
+++ 10_Many_to_Many/src/main/java/com/jpa/model/Course.java
@@ -0,0 +1,53 @@
+package com.jpa.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="course")
+public class Course{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int cid;
+	
+	@Column(name="name")
+	String name;
+
+	@ManyToMany(mappedBy = "course")
+	List<Student> students=new ArrayList<Student>();
+	
+	
+	public int getCid() {
+		return cid;
+	}
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	
+}
